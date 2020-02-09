c07b8cf651c4b84ac59f690fa5b970ab6371d4fe
4dvn <vuha.hau@hyper.vn>
2020-02-09 13:41:36 +0700

    Update Fliper_DJ_MidiUsb.ino

--- Code/Flipper_DJ_Midiusb_2020/Fliper_DJ_MidiUsb.ino
+++ Code/Flipper_DJ_Midiusb_2020/Fliper_DJ_MidiUsb.ino
@@ -22,15 +22,16 @@ CCPotentiometer volumePotentiometers[] = {
   { mux.pin(8), { MIDI_CC::Channel_Volume, CHANNEL_9 } },
   { mux.pin(9), { MIDI_CC::Channel_Volume, CHANNEL_10 } },
 // End 10 Potentiometers
-  
+};
+NoteButton button[]{  
 // Start Button row 3
-  { mux.pin(10), {(47), CHANNEL_1 } },
+  { mux.pin(10), {47, CHANNEL_1 } },
   
 // Start Buttons row 4 
-  { mux.pin(11), { (48), CHANNEL_1 } },
-  { mux.pin(12), { (49), CHANNEL_1 } },
-  { mux.pin(13), { (50), CHANNEL_1 } },
-  { mux.pin(14), { (51), CHANNEL_1 } },
+  { mux.pin(11), {48, CHANNEL_1 } },
+  { mux.pin(12), {49, CHANNEL_1 } },
+  { mux.pin(13), {50, CHANNEL_1 } },
+  { mux.pin(14), {51, CHANNEL_1 } },
 // End Buttons row 4
   
 //  { mux.pin(15), { MIDI_CC::Channel_Volume, CHANNEL_11 } },//  12 Potentiometers eq
@@ -38,19 +39,19 @@ CCPotentiometer volumePotentiometers[] = {
 
 using namespace MIDI_Notes;
 ///Row 1
-NoteButton button1 = {6, {(36), CHANNEL_1},}; // Pin 6 Note C2 on MIDI channel 1
-NoteButton button2 = {7, {(37), CHANNEL_1},};
-NoteButton button3 = {8, {(38), CHANNEL_1},};
-NoteButton button4 = {9, {(39), CHANNEL_1},};
+NoteButton button1 = {6, {36, CHANNEL_1},}; // Pin 6 Note C2 on MIDI channel 1
+NoteButton button2 = {7, {37, CHANNEL_1},};
+NoteButton button3 = {8, {38, CHANNEL_1},};
+NoteButton button4 = {9, {39, CHANNEL_1},};
 ///Row 2
-NoteButton button5 = {10, {(40), CHANNEL_1},}; // Pin 10 Note ... on MIDI channel 1
-NoteButton button6 = {14, {(41), CHANNEL_1},};
-NoteButton button7 = {15, {(42), CHANNEL_1},};
-NoteButton button8 = {16, {(43), CHANNEL_1},};
+NoteButton button5 = {10, {40, CHANNEL_1},}; // Pin 10 Note ... on MIDI channel 1
+NoteButton button6 = {14, {41, CHANNEL_1},};
+NoteButton button7 = {15, {42, CHANNEL_1},};
+NoteButton button8 = {16, {43, CHANNEL_1},};
 ///Row 3
-NoteButton button9 = {A0, {(44), CHANNEL_1},}; // Pin A0 Note ... on MIDI channel 1
-NoteButton button10 = {A1, {(45), CHANNEL_1},};
-NoteButton button11 = {A2, {(46), CHANNEL_1},};
+NoteButton button9 = {A0, {44, CHANNEL_1},}; // Pin A0 Note ... on MIDI channel 1
+NoteButton button10 = {A1, {45, CHANNEL_1},};
+NoteButton button11 = {A2, {46, CHANNEL_1},};
 // This button pinout on mux
 // Method 2 matrix buttons
 /*
@@ -61,7 +62,6 @@ const AddressMatrix<4, 4> addresses = {{
   {40, 41, 42, 43},
   {36, 37, 38, 39},
 }};
-
 NoteButtonMatrix<4, 4> buttonmatrix = {
   {2, 3, 4, 5}, // row pins
   {6, 7, 8, 9},    // column pins
